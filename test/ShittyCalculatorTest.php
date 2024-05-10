681234b4ab4dd6e398651ec0ad2c67aaad3c2456
theoboldalex <theoboldalex@gmail.com>
2024-05-10 18:57:01 +0100

    Adds tests

---
+++ test/ShittyCalculatorTest.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class ShittyCalculatorTest extends TestCase
+{
+    public function testAdd(): void 
+    {
+        // YOLO
+    }
+}
